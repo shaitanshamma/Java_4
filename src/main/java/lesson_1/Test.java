5c1f840727e257b8ab0eac2c883d274e1ffd6dca
shaitanshamma <kurbatov_andr@hotmail.com>
2019-09-13 18:21:03 +0300

    Вспоминаем Java.IO

--- src/main/java/lesson_1/Test.java
+++ src/main/java/lesson_1/Test.java
@@ -1,5 +1,18 @@
 package lesson_1;
 
-public class Test {
+import java.io.*;
 
+public class Test {
+    public static void main(String[] args) throws IOException {
+        File dir = new File("D:\\Projects\\Java_4\\src\\main\\resources\\1");
+        dir.mkdir();
+        File file = new File("D:\\Projects\\Java_4\\src\\main\\resources\\ТЗ.txt");
+        BufferedReader fl = new BufferedReader(new FileReader(file));
+        String str;
+
+        while ((str=fl.readLine())!=null)
+        System.out.println(str);
+        dir.delete();
+        fl.close();
+    }
 }
